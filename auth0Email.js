2bdfd7113b4badd2b54d24dac9c9e987fe9c46dc
campbel94 <campbel94@gmail.com>
2023-09-02 13:36:40 -0700

    added the auth0 JS file for testing purposes. And additional page functionality added.

---
+++ auth0Email.js
@@ -0,0 +1,43 @@
+window.addEventListener("envelopeModuleReady", () => {
+    if (document.cookie.includes("_lr_env=")) {
+        //how do wechange the if statement above to use the ats.retrieveEnvelope() method instead?
+      console.log("envelope already in storage. directPass() not needed.")
+      return;
+    } else {
+      let key;
+      for (let n = 0, len = localStorage.length; n < len; ++n) {
+        const thisKey = localStorage.key(n);
+        if (thisKey.includes("@@auth0spajs@@::")) {
+          key = thisKey;
+          break;
+        }
+      }
+      var email = localStorage.getItem(key);
+      atsenvelopemodule.setAdditionalData({'type':'email','id':email});
+    }
+
+});
+
+
+// window.addEventListener("envelopeModuleReady", () => {
+//     console.log("is this listener event working?");
+//     function directPass() {
+//       if (document.cookie.includes("_lr_env=")) {
+//         console.log("envelope already in storage. directPass() not needed.")
+//         return;
+//       } else {
+//         let key;
+//         for (let n = 0, len = localStorage.length; n < len; ++n) {
+//           const thisKey = localStorage.key(n);
+//           if (thisKey.includes("@@auth0spajs@@::")) {
+//             key = thisKey;
+//             break;
+//           }
+//         }
+//         var email = localStorage.getItem(key);
+//         atsenvelopemodule.setAdditionalData({'type':'email','id':email});
+//       }
+//     };
+
+//     directPass()
+// });
