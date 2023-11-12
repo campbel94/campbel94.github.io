205ddfd4d7716c5adf70b225668035bb08c109ae
Chris Campbell <chriscampbell@Chriss-MacBook-Pro.local>
2023-11-12 12:23:07 -0800

    Complete Update

---
+++ script.js
@@ -0,0 +1,107 @@
+// UI Varaibles
+// email/phone submit button
+const idSubmitButton = document.getElementById('submit-button');
+const hashingButton = document.getElementById('hashing-button');
+const geoButton = document.getElementById('geo-button');
+const domainButton = document.getElementById('domain-button');
+const requestButton = document.getElementById('envelopeRequest-button');
+const responseButton = document.getElementById('envelopeResponse-button');
+const detectButton = document.getElementById('detectButton');
+const directButton = document.getElementById('directButton');
+const invalidateEnvelopeButton = document.getElementById('invalidateEnvelopeButton');
+
+
+// raw identifiers
+let emailInput = document.getElementById('email-input');
+
+// ATS Reponse
+const atsResponseSpan = document.getElementById("atsResponseSpan");
+
+// API Variables
+// hashes
+var SHA256 =  new Hashes.SHA256
+var SHA1 = new Hashes.SHA1
+var MD5 = new Hashes.MD5
+// hashed identifiers
+const emailHashSHA256 = SHA256.hex(emailInput)
+const emailHashSHA1 = SHA1.hex(emailInput)
+const emailHashMD5 = MD5.hex(emailInput)
+// geo API
+
+// Envelope endpoint
+const baseUrl = 'https://api.rlcdn.com/api/identity/v2/envelope'
+const pid = 13671
+
+
+// Helper/Utility Functions
+
+
+// Event Handlers
+async function emailDisplay(evt){
+    if (emailInput.value){
+        // Dispaly Email
+        const emailField = document.getElementById("identifierSpan");
+        emailField.innerHTML = emailInput.value;
+
+        // Display the three hash types
+        const emailHashSHA256 = SHA256.hex(emailInput.value);
+        document.getElementById("sha256Span").innerHTML = emailHashSHA256
+        const emailHashSHA1 = SHA1.hex(emailInput.value);
+        document.getElementById("sha1Span").innerHTML = emailHashSHA1
+        const emailHashMD5 = MD5.hex(emailInput.value);
+        document.getElementById("md5Span").innerHTML = emailHashMD5
+
+        // Display Current geo
+        const currentLocation = await window.axios.get('https://geo.privacymanager.io/');
+        const countrySpan = document.getElementById("countrySpan");
+        countrySpan.innerHTML = currentLocation.data.country;
+        const regionSpan = document.getElementById("regionSpan");
+        regionSpan.innerHTML = currentLocation.data.region;
+
+        // Display Current Domain
+        domainSpan.innerHTML = window.location.origin
+
+        // Show Envelope Request
+        const requestUrl = `${baseUrl}?pid=${pid}&it=4&iv=${emailHashSHA256}&it=4&iv=${emailHashSHA1}&it=4&iv=${emailHashMD5}`;
+        document.getElementById("envelopeRequestSpan").innerHTML = requestUrl;
+
+        // Show Envelope Response
+        const responseSpan = document.getElementById("envelopeResponseSpan");
+        const envelopeResponse = await window.axios.get(requestUrl);
+        responseSpan.textContent = envelopeResponse.data.envelopes[0].value;
+    }
+    
+}
+
+function detectMode(evt) {
+    window.addEventListener("lrEnvelopePresent", async () => {
+        const detectModeResponse = await atsenvelopemodule.retrieveEnvelope()
+        console.log("lrEnvelopePresent: ", detectModeResponse);
+        atsResponseSpan.innerHTML = detectModeResponse;
+    });
+}
+
+function directMode(evt) {}
+
+function invalidateEnvelope(evt) {
+    var envelope = ats.retrieveEnvelope();
+    if (localStorage.getItem("_lr_env") === null) {
+        alert("No envelope in storage!");
+      } else {
+        ats.invalidateEnvelope();
+        atsResponseSpan.innerHTML = "";
+      }
+    // if (typeof(envelope) != 'undefined') {
+    //     ats.invalidateEnvelope();
+    //     atsResponseSpan.innerHTML = "";
+    // } else {
+    //     alert("No envelope in storage!");
+    // }
+}
+
+
+
+// Event Listeners
+idSubmitButton.addEventListener('click', emailDisplay);
+detectButton.addEventListener('click', detectMode);
+invalidateEnvelopeButton.addEventListener('click', invalidateEnvelope);
